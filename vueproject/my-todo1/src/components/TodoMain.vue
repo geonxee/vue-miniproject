7d5c2a407ae7b1eaf2691171e2784bd16e2a195e
geonxee <1913kg@naver.com>
2023-02-09 14:50:24 +0900

    3. 삭제 기능
    - 체크 기능
    - 삭제 기능 추가

--- vueproject/my-todo1/src/components/TodoMain.vue
+++ vueproject/my-todo1/src/components/TodoMain.vue
@@ -4,9 +4,13 @@
     <header><h1>Vue Fire todo1</h1></header>
     <main>
       <div class="todos">
-        <div class="write">
-          <input ref="writeArea" type="text" v-model="inputValue" @keyup.enter="addItem"/>
+        <div class="write" v-if="writeState === 'add'"> <!--등록-->
+          <input ref="writeArea" type="text" v-model="addItemText" @keyup.enter="addItem"/>
           <button class="btn add" @click="addItem">Add</button>
+        </div>
+        <div class="write" v-else> <!--수정-->
+          <input ref="writeArea" type="text" v-model="editItemText" @keyup.enter="editSave"/>
+          <button class="btn add" @click="editSave">Save</button>
         </div>
         <ul class="list">
           <li v-for="(todo, i) in todos" :key="todo.i">
@@ -14,7 +18,7 @@
             <span>
               {{ todo.text }}
               <b>
-                <a href="" class="edit" @click.prevent="edit(i)">Edit</a>
+                <a href="" class="edit" @click.prevent="editShow(i)">Edit</a>
                 <a href="" class="delete" @click.prevent="del(i)">Del</a>
               </b>
             </span>
@@ -31,31 +35,40 @@
 export default {
     data() {
         return {
-            inputValue: '',
+            writeState: 'add',
+            addItemText: '',
+            crrEditItem: '',
             todos:[
                 {text: '공부하기', state: 'yet'},
                 {text: '운동하기', state: 'done'},
                 {text: '글쓰기', state: 'done'},
-            ]
+            ],
+            todosdone:[]
         }
     },
     methods: {
         addItem() {
-            if (this.inputValue === '') return;
-            this.todos.unshift({text: this.inputValue, state: 'yet'})
-            this.inputValue = ''
+            if (this.addItemText === '') return;
+            this.todos.unshift({text: this.addItemText, state: 'yet'})
+            this.addItemText = ''
         },
         checkItem(i) {
             if (this.todos[i].state === 'yet') {this.todos[i].state = 'done'}
             else {this.todos[i].state = 'yet'}
         },
         del(i) {
-            this.todos.splice(i,1)
+            this.todos.splice(i, 1)
         },
-        edit(i) {
-            console.log(this.todos[i].text)
-            this.inputValue = this.todos[i].text
-            this.todos.splice(i,1)
+        editSave() {
+            if (this.editItemText === '') return;
+            this.todos[this.crrEditItem].text = this.editItemText;
+            this.editItemText = ''
+            this.writeState = 'add'
+        },
+        editShow(i) {
+            this.crrEditItem = i
+            this.writeState = 'edit'
+            this.editItemText = this.todos[i].text
         }
     },
     mounted() {
