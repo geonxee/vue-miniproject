edbd6bfe9106466db890b3321167ef9664aa91f6
geonxee <1913kg@naver.com>
2023-02-09 11:51:52 +0900

    1. 할일 목록 리스팅

---
+++ vueproject/my-todo1/src/components/TodoMain.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="page">
+    <header><h1>Vue Fire todo1</h1></header>
+    <main>
+      <div class="todos">
+        <div class="write">
+          <input type="text"/>
+          <button class="btn add">Add</button>
+        </div>
+        <ul class="list">
+          <li v-for="todo in todos" :key="todo.text">
+            <i class="far fa-check-square"></i>
+            <span>
+              {{ todo.text }}
+              <b>
+                <a href="">Edit</a>
+                <a href="">Del</a>
+              </b>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </main>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todos:[
+                {text: '공부하기', state: 'yet'},
+                {text: '운동하기', state: 'done'},
+                {text: '글쓰기', state: 'done'},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
